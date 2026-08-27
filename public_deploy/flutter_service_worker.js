3fcc0b3e3dc20b2c3a75e5460d764a16fc532ff6
Prasethia <prasethiaproductions@gmail.com>
2026-08-27 19:43:21 +0800

    fix: rebuild public_deploy with latest mobile-app web build

--- public_deploy/flutter_service_worker.js
+++ public_deploy/flutter_service_worker.js
@@ -143,7 +143,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "efe9fdb88c8cc9b360b1790462cacadc
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "d362ab962b2af3980f0448de9788c436",
+"flutter_bootstrap.js": "ebfe768abccf4755270582cc3bd5cac6",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
